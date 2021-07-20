cd2b14d430977786c78eadd3ea34df8e799d4833
eddie-code <231071372@qq.com>
2021-07-20 15:48:12 +0800

    paya >> broker_message_log

---
+++ rocketmq-pay-a/src/main/java/com/example/paya/mapping/BrokerMessageLogMapper.java
@@ -0,0 +1,7 @@
+package com.example.paya.mapping;
+
+import com.example.paya.entity.BrokerMessageLog;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BrokerMessageLogMapper extends Mapper<BrokerMessageLog> {
+}
