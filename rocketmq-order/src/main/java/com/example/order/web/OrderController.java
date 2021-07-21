092d5d8b562a62c0bd4cea5a776c53f37ff6ef4a
eddie-code <231071372@qq.com>
2021-07-21 15:09:59 +0800

    dubbo 整合 hystrix 断路器 - 线程池方式限流

--- rocketmq-order/src/main/java/com/example/order/web/OrderController.java
+++ rocketmq-order/src/main/java/com/example/order/web/OrderController.java
@@ -19,13 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
+//    hystrix断路器线程池方式降级
+//    @HystrixCommand(
+//            commandKey = "/createOrder",
+//            fallbackMethod = "createOrderFallbackMethod4Timeout",
+//            commandProperties = {
+//                    @HystrixProperty(name = "execution.timeout.enabled", value = "true"),
+//                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+//            }
+//    )
+
+//    hystrix断路器线程池方式限流
     @HystrixCommand(
             commandKey = "/createOrder",
-            fallbackMethod = "createOrderFallbackMethod",
             commandProperties = {
-                    @HystrixProperty(name = "execution.timeout.enabled", value = "true"),
-                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
-            }
+                    @HystrixProperty(name="execution.isolation.strategy", value="THREAD")
+            },
+            threadPoolKey = "createOrderThreadPool",
+            threadPoolProperties = {
+                    // 并发执行的最大线程数，默认10
+                    @HystrixProperty(name = "coreSize", value = "10"),
+                    // BlockingQueue的最大队列数，默认值-1
+                    @HystrixProperty(name = "maxQueueSize", value = "20000"),
+                    // 即使maxQueueSize没有达到，达到queueSizeRejectionThreshold该值后，请求也会被拒绝，默认值5
+                    @HystrixProperty(name = "queueSizeRejectionThreshold", value = "30")
+            },
+            fallbackMethod = "createOrderFallbackMethod4Thread"
     )
     @RequestMapping("/createOrder/v1")
     public String createOrder(@RequestParam("cityId") String cityId,
@@ -33,17 +52,26 @@ public class OrderController {
                               @RequestParam("userId") String userId,
                               @RequestParam("suppliedId") String suppliedId,
                               @RequestParam("goodsId") String goodsId) throws InterruptedException {
-        Thread.sleep(5000);
+//        Thread.sleep(5000);
         return "下单成功！";
     }
 
-    public String createOrderFallbackMethod(@RequestParam("cityId") String cityId,
+    public String createOrderFallbackMethod4Timeout(@RequestParam("cityId") String cityId,
                                             @RequestParam("platformId") String platformId,
                                             @RequestParam("userId") String userId,
                                             @RequestParam("suppliedId") String suppliedId,
                                             @RequestParam("goodsId") String goodsId) {
         System.out.println("---超时降级策略执行----");
         return "Hystrix timeout!";
+    }
+
+    public String createOrderFallbackMethod4Thread(@RequestParam("cityId")String cityId,
+                                                   @RequestParam("platformId")String platformId,
+                                                   @RequestParam("userId")String userId,
+                                                   @RequestParam("suppliedId")String suppliedId,
+                                                   @RequestParam("goodsId")String goodsId) throws Exception {
+        System.err.println("---线程池限流降级策略执行---");
+        return "hysrtix threadpool !";
     }
 
 }
